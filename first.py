f1982403813f55689c82e43687f6386b082ac42e
umrazkhan017 <106581711+umrazkhan017@users.noreply.github.com>
2022-06-07 18:43:47 +0530

    Update first.py

--- first.py
+++ first.py
@@ -6,3 +6,31 @@ print("Hello World!!!")
 for step in range(5):    
     print(step)
     
+    
+## Python program to illustrate
+# math module
+import math
+
+def Main():
+    num = -85
+
+    # fabs is used to get the absolute 
+    # value of a decimal
+    num = math.fabs(num) 
+    print(num)
+    
+    
+if __name__=="__main__":
+    Main()
+
+   #Ternary Operator --> If else in one statement 
+>>> def find_max(a,b):
+return a if (a>b) else b
+>>> find_max(5, 6)
+6
+
+#Tuples
+Immutable i.e, tuples cannot be changed 
+syntax a=(1,"be",2.5)
+del function throws error 
+    
